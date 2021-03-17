2e08516fe8e147904fc97aa1225d3f7f8bc4b491
rickyg365 <rickyg365@users.noreply.github.com>
2021-03-16 22:31:04 -0700

    Update utoob2.py

--- utoob2.py
+++ utoob2.py
@@ -6,31 +6,40 @@ import time
 import threading
 
 from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
+
+"""
+- Add a playlist Object from pytube so I can group all the links while im offline then when im online 
+  I can batch download everything I have queued up
+"""
+
 
 def clear_screen():
-    os.system("clear")
+    os.system("cls")
+
 
 def load_video(url):
     global yt
     try:
-        yt = YouTube(url, on_progress_callback = load, on_complete_callback = finish)
+        yt = YouTube(url, on_progress_callback=load, on_complete_callback=finish)
     except VideoUnavailable:
         print(f"Video unavailable: {url}")
+
 
 def load(chunk, file_handle, bytes_remaining):
     contentSize = video.filesize
     size = contentSize - bytes_remaining
 
     print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
-    '█' * int(size*20/contentSize), ' '*(20-int(size*20/contentSize)), float(size/contentSize*100)), end='')
-
+    '#' * int(size*20/contentSize), ' '*(20-int(size*20/contentSize)), float(size/contentSize*100)), end='')
+# '█'
 
 def finish(arg1=None, arg2=None):
     print('\r' + '[Download Completed]:D'+28*' ')
 
 
 def startup(thread):
-    clear_screen()
+    # clear_screen()
     count = 0
     states = ["Loading   ", "Loading.  ", "Loading.. ", "Loading..."]
     
@@ -38,17 +47,18 @@ def startup(thread):
         
         if count > 3:
             count = 0
-        elif count == 2:
-            clear_screen()
+        # elif count == 2:
+            # clear_screen()
         
         print('\r' + states[count], end='')
         count += 1
         time.sleep(0.3)
     print("Process done!")
 
+
 url = input("Youtube URL: ")
 path = "~/downloads/"
-
+print("\n")
 th = threading.Thread(target=load_video, args=(url,))
 th.start()
 startup(th)
@@ -63,10 +73,10 @@ description = yt.description
 video = yt.streams.get_lowest_resolution()
 
 # yt.streams.get_lowest_resolution().download()
-clear_screen()
-print(f"{title}\t|{length_min}m {length_sec}s|")
+# clear_screen()
+print(f"{50*'_'}\n{length_min}m {length_sec}s | {title}")
 
-down = input("\n Download?: ")
+down = input("\nWould you like to download?(y/n): ")
 
 print("\n")
 if down.lower() == 'y':
